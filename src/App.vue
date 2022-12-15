19adc7824332fde31e54270ba13e1b6303542cdc
Nil Valero <nvalero13@gmail.com>
2022-12-15 11:45:16 +0100

    lab done

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <NewComponent />
+  <button @click="helloWorld">Button</button>
+</template>
+
+<script setup>
+import NewComponent from './components/NewComponent.vue'
+
+function helloWorld() {
+  console.log("Hello World")
+}
+</script>
+
+<style scoped>
+button {
+  margin-top: 20px;
+  padding: 5px 10px;
+  border: 2px solid black;
+}
+</style>
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h2>This is a New Component</h2>
+        <p>{{ data }}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const data = ref("Hi! this is data from my javascript");
+
+</script>
+
+<style>
+.container {
+    border: 2px solid black;
+    border-radius: 10px;
+    padding: 10px 30px;
+    box-shadow:0px 3px 3px rgba(0, 0, 0, 0.272);
+}
+</style>
